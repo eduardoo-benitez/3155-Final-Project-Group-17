96370f463f06b8187c4d23393f03669b2495b053
Alexander <72211641+agriff93@users.noreply.github.com>
2023-05-02 13:50:36 -0400

    Update Database Group Assignment 4.11.2023.sql

--- MySQL Stuff Week 2/Database Group Assignment 4.11.2023.sql
+++ MySQL Stuff Week 2/Database Group Assignment 4.11.2023.sql
@@ -16,8 +16,11 @@ CREATE TABLE posts (
     rating ENUM('1','2','3','4','5'),
     postInfo TEXT NOT NULL,
     author_id INT NOT NULL,
+    post_id SERIAL,
+    genrePost INT NOT NULL,
     PRIMARY KEY (author_id),
-	FOREIGN KEY (author_id) REFERENCES users(account_id) ON UPDATE CASCADE ON DELETE SET NULL
+	FOREIGN KEY (author_id) REFERENCES users(account_id) ON UPDATE CASCADE ON DELETE SET NULL,
+	FOREIGN KEY (genrePost) REFERENCES tags(genre_id) ON UPDATE CASCADE ON DELETE SET NULL
 );
 
 CREATE TABLE userProfile (
@@ -26,4 +29,11 @@ CREATE TABLE userProfile (
     numPosts INT UNSIGNED AUTO_INCREMENT NOT NULL,
     PRIMARY KEY (account_id),
 	FOREIGN KEY (account_id) REFERENCES users(account_id) ON UPDATE CASCADE ON DELETE SET NULL
+);
+
+CREATE TABLE tags (
+    genre_id INT NOT NULL,
+    genre_name CHAR(40) NOT NULL
+    PRIMARY KEY (genre_id),
+	FOREIGN KEY (genre_id) REFERENCES posts(genrePost) ON UPDATE CASCADE ON DELETE SET NULL
 );
